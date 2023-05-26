9c5eabde2c269e675370dc2ebd19d4d8d6a56323
RONAKON <ronakon.wat@axonstech.com>
2023-05-26 13:09:31 +0700

    first commit

---
+++ src/main/java/com/ronakon45/mykafka/MykafkaApplication.java
@@ -0,0 +1,13 @@
+package com.ronakon45.mykafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MykafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MykafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ronakon45/mykafka/MykafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronakon45.mykafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MykafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
